b8a2f2c7095647f366029497db5a8f5500477876
Tiago <tiagotele@gmail.com>
2015-07-08 01:59:16 -0300

    Changing the type of main Activity to ListActivity. Overriding funciont to work with click on item of listItem.

--- app/src/main/java/com/example/tiago/androidlistviewexample/MainActivity.java
+++ app/src/main/java/com/example/tiago/androidlistviewexample/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.tiago.androidlistviewexample;
 
 import android.app.ListActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBarActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
-    private ListView tweetListView;
     private String[] stringArray ;
     private ArrayAdapter tweetItemArrayAdapter;
 
@@ -22,18 +22,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        stringArray = new String[10];
-        for(int i=0; i < stringArray.length; i++){
-            stringArray[i] = "String " + i;
-        }
-        //tweetItemArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
-        tweetItemArrayAdapter = new TweetAdapter(this, stringArray);
-
-        tweetListView = (ListView) findViewById(R.id.tweetList);
-
-
         tweetItemArrayAdapter = new TweetAdapter(this, new String[10]);
-        tweetListView.setAdapter(tweetItemArrayAdapter);
+        setListAdapter(tweetItemArrayAdapter);
 
     }
 
@@ -57,5 +47,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Log.d("INFO","Position: "+position+" id: "+id);
+        TextView t = (TextView) v.findViewById(R.id.tweetTitle);
+        if(t.getText().equals("Header Text")){
+            t.setText("Tweet Clicked");
+        }else{
+            t.setText("Header Text");
+        }
+
     }
 }
